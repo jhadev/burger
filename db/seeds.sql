c2cb38e8eca9cc26b931e2803282306c730d951d
JA <joshappeldev@gmail.com>
2018-10-24 18:58:31 -0400

    first

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name, eaten) VALUES ('Bacon Cheddar Ranch', true);
+INSERT INTO burgers (name, eaten) VALUES ('Jalapeno Pepperjack Cheeseburger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Baconator', true);
+INSERT INTO burgers (name) VALUES ('Mushroom Swiss Burger');
